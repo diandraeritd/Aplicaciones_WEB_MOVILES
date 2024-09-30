0e83f243e1af130296b8e325a2f8bcc923c298b0
DIANDRA CHUQUILLANQUI VENTURA <152913033+diandraeritd@users.noreply.github.com>
2024-09-30 12:51:08 -0500

    Update primero.dart

--- SEM03/primero.dart
+++ SEM03/primero.dart
@@ -1,3 +1,4 @@
+//Diseñe un programa en Lenguaje Dart que determine el área total (A) y el volumen (V) de un cilindro del que se conoce su radio (r) y su altura (h). Considere las siguientes fórmulas: A = 2πr(r+h) y V = π r²h
 void main() {
   double lado = 8.0;
 
